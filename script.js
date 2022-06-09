601ca223e45d8899be15cb9d1cb441716fe0532d
rosousa <rodolfozsousa@gmail.com>
2022-06-09 01:41:50 -0300

    Items sum finished

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let food = false;
 let drink = false;
 let dessert= false;
+let food_price;
+let drink_price;
+let dessert_price;
 
 function food_choice(class_name){
     let first_food = document.querySelector('.first-food');
@@ -11,14 +14,23 @@ function food_choice(class_name){
         first_food.classList.add('selected');
         second_food.classList.remove('selected');
         third_food.classList.remove('selected');
+        food_price = document.querySelector('.first-food .food-price').textContent;
+        food_price = food_price.replace('R$ ', '').replace(',', '.');
+        food_price = parseFloat(food_price);
     } else if (class_name.includes('second-food')){
         second_food.classList.add('selected');
         first_food.classList.remove('selected');
         third_food.classList.remove('selected');
+        food_price = document.querySelector('.second-food .food-price').textContent;
+        food_price = food_price.replace('R$ ', '').replace(',', '.');
+        food_price = parseFloat(food_price);
     } else if (class_name.includes('third-food')){
         third_food.classList.add('selected');
         first_food.classList.remove('selected');
         second_food.classList.remove('selected');
+        food_price = document.querySelector('.third-food .food-price').textContent;
+        food_price = food_price.replace('R$ ', '').replace(',', '.');
+        food_price = parseFloat(food_price);
     }
     food = true;
     checkout();
@@ -33,14 +45,23 @@ function drink_choice(class_name){
         first_drink.classList.add('selected');
         second_drink.classList.remove('selected');
         third_drink.classList.remove('selected');
+        drink_price = document.querySelector('.first-drink .drink-price').textContent;
+        drink_price = drink_price.replace('R$ ', '').replace(',', '.');
+        drink_price = parseFloat(drink_price);
     } else if (class_name.includes('second-drink')){
         second_drink.classList.add('selected');
         first_drink.classList.remove('selected');
         third_drink.classList.remove('selected');
+        drink_price = document.querySelector('.second-drink .drink-price').textContent;
+        drink_price = drink_price.replace('R$ ', '').replace(',', '.');
+        drink_price = parseFloat(drink_price);
     } else if (class_name.includes('third-drink')){
         third_drink.classList.add('selected');
         first_drink.classList.remove('selected');
         second_drink.classList.remove('selected');
+        drink_price = document.querySelector('.third-drink .drink-price').textContent;
+        drink_price = drink_price.replace('R$ ', '').replace(',', '.');
+        drink_price = parseFloat(drink_price);
     }
     drink = true;
     checkout();
@@ -55,14 +76,23 @@ function dessert_choice(class_name){
         first_dessert.classList.add('selected');
         second_dessert.classList.remove('selected');
         third_dessert.classList.remove('selected');
+        dessert_price = document.querySelector('.first-dessert .dessert-price').textContent;
+        dessert_price = dessert_price.replace('R$ ', '').replace(',', '.');
+        dessert_price = parseFloat(dessert_price);
     } else if (class_name.includes('second-dessert')){
         second_dessert.classList.add('selected');
         first_dessert.classList.remove('selected');
         third_dessert.classList.remove('selected');
+        dessert_price = document.querySelector('.second-dessert .dessert-price').textContent;
+        dessert_price = dessert_price.replace('R$ ', '').replace(',', '.');
+        dessert_price = parseFloat(dessert_price);
     } else if (class_name.includes('third-dessert')){
         third_dessert.classList.add('selected');
         first_dessert.classList.remove('selected');
         second_dessert.classList.remove('selected');
+        dessert_price = document.querySelector('.third-dessert .dessert-price').textContent;
+        dessert_price = dessert_price.replace('R$ ', '').replace(',', '.');
+        dessert_price = parseFloat(dessert_price);
     }
     dessert = true;
     checkout();
@@ -71,5 +101,6 @@ function dessert_choice(class_name){
 function checkout(){
     if(food && drink && dessert){
         console.log('Finalizando pedido!');
+        console.log((food_price + drink_price + dessert_price).toFixed(2));
     }
 }
